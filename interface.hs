0e56e8fba69f6714719bc2a05a07100125b26880
Vladi Stoyanov <vladisto100@gmail.com>
2022-01-14 16:47:48 +0200

    Working interface, remains to do path with <..> and readInput

--- interface.hs
+++ interface.hs
@@ -6,14 +6,18 @@ module Interface where
     import qualified Data.Text as T
 
 
-    -- run :: pathStr -> IO()
-    -- run currentPath = do
-    --         cmd <- getline
-    --         if (cmd == "")
-    --             then run currentPath 
-    --         else if cmd == "exit"
-    --             then return()
-    --           --  else do
+    run :: PathStr -> SystemElement -> IO ()
+    run currentPath currentRoot = do
+            cmd <- getLine
+            if (cmd == "")
+                then run currentPath currentRoot
+            else if cmd == "exit"
+                then return()
+            else do
+                (newPath, newRoot) <- processInput cmd currentPath currentRoot
+                putStr (newPath ++ "> " )
+                run newPath newRoot
+
 
     processInput :: String -> PathStr -> SystemElement -> IO (PathStr, SystemElement)
     processInput inputArr currentPath currentRoot = runCmd (head $ words inputArr) (tail $ words inputArr) currentPath currentRoot
@@ -22,7 +26,14 @@ module Interface where
     runCmd "pwd" _ currentPath currentRoot = printWorkingDirectory currentPath currentRoot
     runCmd "cd" args currentPath  currentRoot = changeDirCommand args currentPath currentRoot
     runCmd "ls" args currentPath  currentRoot = listContentsCommand args currentPath currentRoot
-    --runCmd "cat" args currentPath currentRoot = concatenateFilesCommand args currentPath currentRoot
+    runCmd "cat" args currentPath currentRoot = concatenateFilesCommand args currentPath currentRoot
+    runCmd "rm" args currentPath currentRoot = removeFileCommand args currentPath currentRoot
+    runCmd "getRoot" args currentPath currentRoot = do print currentRoot
+                                                       return(currentPath, currentRoot)
+    runCmd "print" args currentPath currentRoot = do putStrLn (concat args)
+                                                     return(currentPath, currentRoot)
+    runCmd  _   _    currentPath currentRoot = do putStrLn "command not found"
+                                                  return(currentPath, currentRoot)
 
     printWorkingDirectory :: PathStr -> SystemElement -> IO (PathStr, SystemElement)
     printWorkingDirectory currentPath currentRoot = do
@@ -37,7 +48,7 @@ module Interface where
                                                 putStrLn "The system cannot find the path specified"
                                                 return (currentPath, currentRoot)
     changeDirCommand _ currentPath currentRoot = do
-                                            putStrLn "Too many arguments"
+                                            putStrLn "Invalid arguments"
                                             return (currentPath, currentRoot)
 
 --cd scheme/../haskell/../scheme/../haskell е еквивалентно на cd haskell
@@ -52,16 +63,22 @@ module Interface where
 
     concatenateFilesCommand :: [String] -> PathStr -> SystemElement -> IO (PathStr, SystemElement)
     -- "Folder1 Folder1 Folder1 Folder1 Folder1 "
-    concatenateFilesCommand [files] currentPath currentRoot 
+    concatenateFilesCommand args currentPath currentRoot
         | null outputFile =  do putStrLn $ concatMap (\x -> getContent(goToPath (getFullPath currentPath x) currentRoot)) inputFilePaths
                                 return (currentPath, currentRoot)
         | otherwise = return (currentPath, concatenateFiles inputFilePaths (head outputFile) currentPath currentRoot)
-                                      where filesList = words files
+                                      where filesList = args
                                             inputFilePaths = takeWhile (/= ">") filesList
-                                            outputFile = tail $ dropWhile (/= ">") filesList --head is ">"
+                                            outputFile = if null $ dropWhile (/= ">") filesList
+                                                            then []
+                                                            else tail $ dropWhile (/= ">") filesList--head is ">"
+
+    removeFileCommand :: [String] -> PathStr -> SystemElement -> IO (PathStr, SystemElement)
+    removeFileCommand [files] currentPath currentRoot = return(currentPath, removeFile pathToRemoveFrom currentDir)
+        where pathToRemoveFrom = convertPathToArr(getFullPath currentPath files)
+              currentDir = goToPath currentPath currentRoot
 
     readInput :: [Char] -> String -> IO()
     readInput input "." = print input
     readInput input currentLine = do currInput <- getLine
                                      readInput (input ++ currentLine) currInput
-        
